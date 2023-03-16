20591a8f24f40b1558c55380b9d30502b8449f74
Dayron Remigio Hernández <dyronrh@yahoo.es>
2023-03-16 18:10:40 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -8,12 +8,3 @@ output "project_id" {
   description = "GCloud Project ID"
 }
 
-output "kubernetes_cluster_name" {
-  value       = google_container_cluster.primary.name
-  description = "GKE Cluster Name"
-}
-
-output "kubernetes_cluster_host" {
-  value       = google_container_cluster.primary.endpoint
-  description = "GKE Cluster Host"
-}
