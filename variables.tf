17aadc350fc772141142ca154e37862ca24ba484
Dayron Remigio Hernandez (GLOBANT) <dayronremigio.globant@latam.com>
2023-03-16 17:42:33 -0300

    add

---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "project_id" {
+    type        = string
+    description = "define the project on gcp"
+    default     = ""
+}
+
+variable "name" {
+    type        = string
+    description = "define the name of  gcp backup object"
+    default     = null
+    required = true
+}
+
+variable "region" {
+    type        = string
+    description = "define project region"
+    default     = null
+}
+
+variable "region_bck" {
+    type        = string
+    description = "define backup project region"
+    default     = null
+}
+
+
+variable "zone" {
+    type        = string
+    description = "define project zone"
+    default     = ""
+}
+
+variable "vpc_network" {
+    type        = string
+    description = "select the vpc"
+    default     = ""
+}
+
+variable "sub_network" {
+    type        = string
+    description = "define sub network"
+    default     = ""
+}
+
+
+
+variable "cluster_resource_labels" {
+  type = "map"
+  default = {
+   # "cmdb" = "backup-",
+  }
+}
